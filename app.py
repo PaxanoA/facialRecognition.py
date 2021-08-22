d9fab6f1932e507809d92b101617ad1111b96b1a
PaxanoA <72090162+PaxanoA@users.noreply.github.com>
2021-08-22 00:16:32 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,45 @@
+import cv2
+import os
+import imutils
+
+personName = "Andres"
+dataPath = "C:/Users/titip/PycharmProjects/openCvpython/Data"
+personPath = dataPath + "/" + personName
+
+if not os.path.exists(personPath):
+    print("Carpeta creada")
+    os.makedirs(personPath)
+
+cap = cv2.VideoCapture('Resources/Video Proyecto de Grado.mp4')
+
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+count = 0
+
+while True:
+    ret, frame = cap.read()
+    if not ret: break
+    frame = imutils.resize(frame, width=640)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    auxFrame = frame.copy()
+
+    faces = faceClassif.detectMultiScale(gray,1.3,5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, h), (0, 255, 0))
+        face = auxFrame[y:y + h, x: x + w]
+        face = cv2.resize(face, (150, 150), interpolation=cv2.INTER_CUBIC) #determinar tamaño de los frames // determine frame size
+        cv2.imwrite(personPath + '/face_{}.jpg'.format(count), face) #conteo de los frames // frame count
+        count = count + 1
+    cv2.imshow("Frame", frame)
+    k = cv2.waitKey(1)
+    if k == 27 or count >= 300: #tomar 300 frames // take 300 frames (0-299)
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+#archivo creado por Andres Pachano
+#su uso queda permitido con fines educativos mas no comerciales
+
+#script created by Andres Pachano
+#the usage of this script is aviable with educative proposes but with no commercial proposes
